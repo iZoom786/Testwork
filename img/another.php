f5b948aa0ac1b57cdbe4b1a9d50e93aa426a1f88
iZoom786 <asifparacha786@gmail.com>
2022-03-07 03:59:54 +0500

    Update another.php

--- img/another.php
+++ img/another.php
@@ -1,31 +1,15 @@
-//This is the minimal code for an image upload for first time learners
-//html portion
-<!DOCTYPE html>
-<html>
-<head>
-	<title>ImageUpload</title>
-</head>
-<body>
-	<form action="upload.php" method="post" enctype="multipart/form-data">
-		<label>Username</label>
-		<input type="text" name="username">
-		<br>
-		<label>UploadImage</label>
-		<input type="file" name='myfile'>
-		<br/>
-		<input type="submit" value="upload">
-	</form>
-</body>
-</html>
-  
- //php portion
-  <?php
-	$user=$_POST['username'];
-	$image=$_FILES['myfile'];
-	echo "Hello $user <br/>";
-	echo "File Name<b>::</b> ".$image['name'];
-
-	move_uploaded_file($image['tmp_name'],$image['name']);
-	//here the "photos" folder is in same folder as the upload.php, 
-	//otherwise complete url has to be mentioned
-	?>
+<?php
+if(isset($_POST['btn-upload']))
+{    
+     
+ $file = rand(1000,100000)."-".$_FILES['file']['name'];
+    $file_loc = $_FILES['file']['tmp_name'];
+ $file_size = $_FILES['file']['size'];
+ $file_type = $_FILES['file']['type'];
+ $folder="/";
+ 
+ move_uploaded_file($file_loc,$folder.$file);
+ $sql="INSERT INTO tbl_uploads(file,type,size) VALUES('$file','$file_type','$file_size')";
+ mysql_query($sql); 
+}
+?>
