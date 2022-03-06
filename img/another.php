8c71eb03ecb35c56b163b4289505393ca87b2934
iZoom786 <asifparacha786@gmail.com>
2022-03-07 03:31:07 +0500

    Update another.php

--- img/another.php
+++ img/another.php
@@ -25,7 +25,7 @@
 	echo "Hello $user <br/>";
 	echo "File Name<b>::</b> ".$image['name'];
 
-	move_uploaded_file($image['tmp_name'],"".$image['name']);
+	move_uploaded_file($image['name'],"".$image['name']);
 	//here the "photos" folder is in same folder as the upload.php, 
 	//otherwise complete url has to be mentioned
 	?>
